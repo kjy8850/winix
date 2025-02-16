5c506ce40773e10530bc2306d88ee2f1eaa371a0
kjy8850 <almexf88@gmail.com>
2025-02-17 04:36:13 +0900

    Update driver.py

--- custom_components/winix/driver.py
+++ custom_components/winix/driver.py
@@ -1,5 +1,3 @@
-"""The WinixDriver component."""
-
 from __future__ import annotations
 
 import logging
@@ -24,28 +22,32 @@ class WinixDriver:
 
     category_keys = {
         "power": "D02",
-        "mode": "A03",
-        "airflow": "A04",
-        "aqi": "A05",
-        "plasma": "A07",
-        "filter_hour": "A21",
-        "air_quality": "S07",
-        "air_qvalue": "S08",
-        "ambient_light": "S14",
+        "mode": "D03",
+        "fan_speed": "D04",
+        "target_humidity": "D05",
+        "child_lock": "D08",
+        "current_humidity": "D10",
+        "uv_sterilization": "D13",
+        "timer": "D15",
     }
 
     state_keys = {
         "power": {"off": "0", "on": "1"},
-        "mode": {"auto": "01", "manual": "02"},
-        "airflow": {
-            "low": "01",
-            "medium": "02",
-            "high": "03",
-            "turbo": "05",
-            "sleep": "06",
+        "mode": {
+            "auto": "01",
+            "manual": "02",
+            "laundry_dry": "03",
+            "shoes_dry": "04",
+            "silent": "05",
+            "continuous": "06"
         },
-        "plasma": {"off": "0", "on": "1"},
-        "air_quality": {"good": "01", "fair": "02", "poor": "03"},
+        "fan_speed": {
+            "high": "01",
+            "low": "02",
+            "turbo": "03",
+        },
+        "child_lock": {"off": "0", "on": "1"},
+        "uv_sterilization": {"off": "0", "on": "1"},
     }
 
     def __init__(self, device_id: str, client: aiohttp.ClientSession) -> None:
@@ -65,59 +67,35 @@ class WinixDriver:
             self.category_keys["power"], self.state_keys["power"]["on"]
         )
 
-    async def auto(self):
-        """Set device in auto mode."""
+    async def set_mode(self, mode):
+        """Set device mode."""
         await self._rpc_attr(
-            self.category_keys["mode"], self.state_keys["mode"]["auto"]
+            self.category_keys["mode"], self.state_keys["mode"].get(mode, "01")
         )
 
-    async def manual(self):
-        """Set device in manual mode."""
+    async def set_fan_speed(self, speed):
+        """Set fan speed."""
         await self._rpc_attr(
-            self.category_keys["mode"], self.state_keys["mode"]["manual"]
+            self.category_keys["fan_speed"], self.state_keys["fan_speed"].get(speed, "01")
         )
 
-    async def plasmawave_off(self):
-        """Turn plasmawave off."""
-        await self._rpc_attr(
-            self.category_keys["plasma"], self.state_keys["plasma"]["off"]
-        )
+    async def set_humidity(self, humidity):
+        """Set target humidity."""
+        await self._rpc_attr(self.category_keys["target_humidity"], str(humidity))
 
-    async def plasmawave_on(self):
-        """Turn plasmawave on."""
-        await self._rpc_attr(
-            self.category_keys["plasma"], self.state_keys["plasma"]["on"]
-        )
+    async def set_timer(self, timer):
+        """Set timer."""
+        await self._rpc_attr(self.category_keys["timer"], str(timer))
 
-    async def low(self):
-        """Set speed low."""
-        await self._rpc_attr(
-            self.category_keys["airflow"], self.state_keys["airflow"]["low"]
-        )
+    async def set_child_lock(self, lock: bool):
+        """Enable or disable child lock."""
+        value = "1" if lock else "0"
+        await self._rpc_attr(self.category_keys["child_lock"], value)
 
-    async def medium(self):
-        """Set speed medium."""
-        await self._rpc_attr(
-            self.category_keys["airflow"], self.state_keys["airflow"]["medium"]
-        )
-
-    async def high(self):
-        """Set speed high."""
-        await self._rpc_attr(
-            self.category_keys["airflow"], self.state_keys["airflow"]["high"]
-        )
-
-    async def turbo(self):
-        """Set speed turbo."""
-        await self._rpc_attr(
-            self.category_keys["airflow"], self.state_keys["airflow"]["turbo"]
-        )
-
-    async def sleep(self):
-        """Set device in sleep mode."""
-        await self._rpc_attr(
-            self.category_keys["airflow"], self.state_keys["airflow"]["sleep"]
-        )
+    async def set_uv_sterilization(self, uv: bool):
+        """Enable or disable UV sterilization."""
+        value = "1" if uv else "0"
+        await self._rpc_attr(self.category_keys["uv_sterilization"], value)
 
     async def _rpc_attr(self, attr: str, value: str):
         _LOGGER.debug("_rpc_attr attribute=%s, value=%s", attr, value)
@@ -128,86 +106,30 @@ class WinixDriver:
         raw_resp = await resp.text()
         _LOGGER.debug("_rpc_attr response=%s", raw_resp)
 
-    async def get_filter_life(self) -> int | None:
-        """Get the total filter life."""
-        response = await self._client.get(
-            self.PARAM_URL.format(deviceid=self.device_id)
-        )
-        json = await response.json()
-
-        # pylint: disable=pointless-string-statement
-        """
-        {
-            'statusCode': 200, 'headers': {'resultCode': 'S100', 'resultMessage': ''},
-            'body': {
-                'deviceId': '847207352CE0_364yr8i989', 'totalCnt': 1,
-                'data': [
-                    {
-                        'apiNo': 'A240', 'apiGroup': '004', 'modelId': 'C545', 'attributes': {'P01': '6480'}
-                    }
-                ]
-            }
-        }
-        """
-
-        try:
-            attributes = json["body"]["data"][0]["attributes"]
-            if attributes:
-                return int(attributes["P01"])
-        except Exception:  # pylint: disable=broad-except # noqa: BLE001
-            return None
-
     async def get_state(self) -> dict[str, str]:
         """Get device state."""
-
-        # All devices seem to have max 9 months filter life so don't need to call this API.
-        # await self.get_filter_life()
-
         response = await self._client.get(
             self.STATE_URL.format(deviceid=self.device_id)
         )
         json = await response.json()
 
-        # pylint: disable=pointless-string-statement
-        """
-        {
-            'statusCode': 200,
-            'headers': {'resultCode': 'S100', 'resultMessage': ''},
-            'body': {
-                'deviceId': '847207352CE0_364yr8i989', 'totalCnt': 1,
-                'data': [
-                    {
-                        'apiNo': 'A210', 'apiGroup': '001', 'deviceGroup': 'Air01', 'modelId': 'C545',
-                        'attributes': {'A02': '0', 'A03': '01', 'A04': '01', 'A05': '01', 'A07': '0', 'A21': '1257', 'S07': '01', 'S08': '74', 'S14': '121'},
-                        'rssi': '-55', 'creationTime': 1673449200634, 'utcDatetime': '2023-01-11 15:00:00', 'utcTimestamp': 1673449200
-                    }
-                ]
-            }
-        }
-        """
-
         output = {}
-
         try:
             _LOGGER.debug(json)
             payload = json["body"]["data"][0]["attributes"]
-        except Exception as err:  # pylint: disable=broad-except # noqa: BLE001
+        except Exception as err:
             _LOGGER.error(
                 "Error parsing response json, received %s", json, exc_info=err
             )
-
-            # Return empty object so that callers don't crash (#37)
             return output
 
         for payload_key, attribute in payload.items():
             for category, local_key in self.category_keys.items():
                 if payload_key == local_key:
-                    # pylint: disable=consider-iterating-dictionary
                     if category in self.state_keys:
                         for value_key, value in self.state_keys[category].items():
                             if attribute == value:
                                 output[category] = value_key
                     else:
                         output[category] = int(attribute)
-
         return output
